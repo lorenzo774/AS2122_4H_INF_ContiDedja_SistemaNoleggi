a8dac377c4c258e9fc1162a42ab86befb3a86237
SimonDedja <simon.dedja.15@gmail.com>
2022-02-28 10:30:50 +0100

    Aggiunta Page per la creazione di un furgone

---
+++ SistemaNoleggi_UWP/AddFurgonePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Noleggio_Library;
+
+namespace SistemaNoleggi_UWP
+{
+    public sealed partial class AddFurgonePage : Page
+    {
+        public AddFurgonePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnBtnSalva_Click(object sender, RoutedEventArgs e)
+        {
+            string targa = txtTarga.Text;
+            string modello = txtModello.Text;
+            decimal costoVeicolo = Convert.ToDecimal(txtCostoVeicolo.Text);
+            decimal tariffaGiornaliera = Convert.ToDecimal(txtTariffa.Text);
+            int capacita = int.Parse(txtCapacita.Text);
+
+            SistemaNoleggi.Instance.AggiungiFurgone(targa, modello, costoVeicolo, tariffaGiornaliera, capacita);
+
+            Frame.GoBack();
+        }
+
+        private void OnBtnAnnulla_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
